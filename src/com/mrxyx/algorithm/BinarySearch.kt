3b6752d677dd2572b1bce7321c83e4dd97342139
Xyx <739075802@qq.com>
2021-05-15 01:03:14 +0800

    binary search
    search、range

---
+++ src/com/mrxyx/algorithm/BinarySearch.kt
@@ -0,0 +1,62 @@
+package com.mrxyx.algorithm
+
+class BinarySearch {
+
+    /**
+     * 二分查找
+     * https://leetcode-cn.com/problems/binary-search/
+     */
+    fun binarySearch(nums: IntArray, target: Int): Int {
+        if (nums.isEmpty()) return -1
+        var left = 0
+        var right = nums.size - 1
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            when {
+                nums[mid] == target -> return mid
+                nums[mid] < target -> left = mid + 1
+                nums[mid] > target -> right = mid - 1
+            }
+        }
+        return -1
+    }
+
+    /**
+     * 在排序数组中查找元素的第一个和最后一个位置
+     * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+     */
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        return intArrayOf(leftBound(nums, target), rightBound(nums, target))
+    }
+
+    private fun leftBound(nums: IntArray, target: Int): Int {
+        if (nums.isEmpty()) return -1
+        var left = 0
+        var right = nums.size - 1
+        var mid: Int
+        while (left <= right) {
+            mid = left + (right - left) / 2
+            when {
+                nums[mid] > target || nums[mid] == target -> right = mid - 1
+                nums[mid] < target -> left = mid + 1
+            }
+        }
+        return left
+    }
+
+    private fun rightBound(nums: IntArray, target: Int): Int {
+        if (nums.isEmpty()) return -1
+        var left = 0
+        var right = nums.size - 1
+        var mid: Int
+        while (left <= right) {
+            mid = left + (right - left) / 2
+            when {
+                nums[mid] < target -> left = mid + 1
+                nums[mid] > target -> right = mid - 1
+                nums[mid] == target -> left = mid + 1
+            }
+        }
+        return right
+    }
+}
--- src/com/mrxyx/algorithm/Main.java
+++ src/com/mrxyx/algorithm/Main.java
@@ -1,5 +1,7 @@
 package com.mrxyx.algorithm;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,12 +14,19 @@ public class Main {
 
         //零钱兑换
         Coin coin = new Coin();
-        Integer[] coins = new Integer[]{1,3,4,9};
+        Integer[] coins = new Integer[]{1, 3, 4, 9};
         System.out.println(coin.coinChangeV1(coins, 20));
         System.out.println(coin.coinChangeV2(coins, 20));
         System.out.println(coin.coinChangeV3(coins, 20));
 
+        //N 皇后
         NQueen queen = new NQueen();
         System.out.println(queen.getQueenResult(10));
+
+        //二分查找
+        BinarySearch search = new BinarySearch();
+        int[] nums = new int[]{-1, 0, 2, 3, 4, 4, 4, 6};
+        System.out.println(search.binarySearch(nums, 12));
+        System.out.println(Arrays.toString(search.searchRange(nums, 4)));
     }
 }
